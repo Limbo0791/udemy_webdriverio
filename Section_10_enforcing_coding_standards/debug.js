c90ec4594f08e2b1ef1d32092a4e076024625b36
Wilco Mellema <wilco.mellema@ns.nl>
2023-05-05 09:50:59 +0200

    completed ESLint lectures

--- Section_10_enforcing_coding_standards/debug.js
+++ Section_10_enforcing_coding_standards/debug.js
@@ -4,8 +4,9 @@ function btnClicked() {
 }
 
 function createString(someString) {
-  document.getElementById('p2').innerText =
-    'Characters typed: ' + someString.length;
+  document.getElementById(
+    'p2'
+  ).innerText = `Characters typed: ${someString.length}`;
 }
 
 function charsTyped() {
@@ -14,8 +15,6 @@ function charsTyped() {
   createString(chars);
 }
 
-document.getElementById('b2').addEventListener('click', btn2Clicked);
-
 function btn2Clicked() {
   const main = document.getElementById('main');
   const child = document.getElementById('p3');
@@ -23,12 +22,14 @@ function btn2Clicked() {
   main.removeChild(child);
 }
 
-document.getElementById('i2').addEventListener('change', valueEntered);
+document.getElementById('b2').addEventListener('click', btn2Clicked);
 
 function valueEntered() {
   const size = document.getElementById('i2').value.length;
-
-  document.getElementById('p4').style.fontSize = size + 'em';
+  document.getElementById('p4').style.fontSize = `${size} em`;
 }
 
+document.getElementById('i2').addEventListener('change', valueEntered);
+
 btnClicked();
+charsTyped();
